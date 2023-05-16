2d71ac07cc9fb1d53df728ebc208dc67d57a19b4
murachova <veraverapatoka@gmail.com>
2023-05-16 17:30:25 +0200

    first commit

---
+++ cypress/e2e/before-after.cy.js
@@ -0,0 +1,94 @@
+describe("Before method", () => {
+
+  before(() => {
+    cy.visit('https://sanitarskyi-ngx-admin.herokuapp.com/');
+    cy.get('[src="assets/images/material-dark-theme.jpg"]').click();
+    cy.get('[title="Forms"]').click();
+    cy.get('[title="Form Layouts"]').click();
+  })
+
+  it("Test 1", () => {
+    cy.get('[placeholder="Jane Doe"]').type('Hello');
+  })
+  
+  it("Test 2", () => {
+    // cy.visit('https://sanitarskyi-ngx-admin.herokuapp.com/');
+    // cy.get('[src="assets/images/material-dark-theme.jpg"]').click();
+    // cy.get('[title="Forms"]').click();
+    // cy.get('[title="Form Layouts"]').click();
+
+    cy.get('[placeholder="Jane Doe"]').type('Hello');
+  })
+
+})
+
+describe("Before each method", () => {
+
+  beforeEach(() => {
+    cy.visit('https://sanitarskyi-ngx-admin.herokuapp.com/');
+    cy.get('[src="assets/images/material-dark-theme.jpg"]').click();
+    cy.get('[title="Forms"]').click();
+    cy.get('[title="Form Layouts"]').click();
+  })
+
+  it("Test 1", () => {
+    cy.get('[placeholder="Jane Doe"]').type('Hello');
+  })
+  
+  it("Test 2", () => {
+    cy.get('[placeholder="Jane Doe"]').type('Hello');
+  })
+
+
+})
+
+
+describe("After method", () => {
+
+  beforeEach(() => {
+    cy.visit('https://sanitarskyi-ngx-admin.herokuapp.com/');
+
+    cy.log(`Open "Form Layouts" page ...`);
+    cy.get('[src="assets/images/material-dark-theme.jpg"]').click();
+    cy.get('[title="Forms"]').click();
+    cy.get('[title="Form Layouts"]').click();
+  })
+
+  after(() => {
+    cy.log("HELLO")
+  })
+
+  it("Test 1", () => {
+    cy.get('[placeholder="Jane Doe"]').type('Hello');
+  })
+  
+  it("Test 2", () => {
+    cy.get('[placeholder="Jane Doe"]').type('Hello');
+  })
+})
+
+
+
+describe.only("After each method", () => {
+
+  beforeEach(() => {
+    cy.visit('https://sanitarskyi-ngx-admin.herokuapp.com/');
+
+    cy.log(`Open "Form Layouts" page ...`);
+    cy.get('[src="assets/images/material-dark-theme.jpg"]').click();
+    cy.get('[title="Forms"]').click();
+    cy.get('[title="Form Layouts"]').click();
+  })
+
+  afterEach(() => {
+    cy.log("HELLO")
+  })
+
+  it("Test 1", () => {
+    cy.get('[placeholder="Jane Doe"]').type('Hello');
+  })
+  
+  it("Test 2", () => {
+    cy.get('[placeholder="Jane Doe"]').type('Hello');
+  })
+})
